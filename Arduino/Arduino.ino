4381e1bb45c4942bf4d40f2c215978419bbd3acf
ThiagoTGM <thiagotgmgaming@gmail.com>
2018-10-14 06:00:48 -0700

    Implemented Bluetooth support on the hardware side.

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,5 +1,3 @@
-#include <mutex>
-
 #include <Servo.h>
 
 #define DOOR_COUNT 2
@@ -8,15 +6,21 @@ const int LED_PINS[] = { 3, 5 };
 const int SERVO_PINS[] = { 11, 12 };
 
 #define FLASH_DELAY 100
-#define MOVE_DELAY 50
+#define MOVE_DELAY 10
+#define MOVE_INCREMENT 1
 
 #define CLOSED_ANGLE 0
-#define OPEN_ANGLE 80
+#define OPEN_ANGLE 30
 
 Servo doors[DOOR_COUNT];
-volatile bool moving[DOOR_COUNT];
+volatile bool shouldToggle[DOOR_COUNT];
 bool closed[DOOR_COUNT];
 
+/*
+ * Sets the state of the LEDs in the board.
+ * 
+ * state - The state to set the LEDs to.
+ */
 void setLeds( int state ) {
 
   for ( const int ledPin : LED_PINS ) {
@@ -27,6 +31,11 @@ void setLeds( int state ) {
   
 }
 
+/*
+ * Flashes all the LEDs in the board.
+ * 
+ * n - The amount of times to flash.
+ */
 void flash( int n ) {
 
   for ( int i = 0; i < n; i++ ) {
@@ -43,52 +52,66 @@ void flash( int n ) {
   
 }
 
+/*
+ * Opens a door.
+ * 
+ * doorN - The number of the door to open.
+ */
 void openDoor( int doorN ) {
 
-  noInterrupts();
-  if ( moving[doorN] || !closed[doorN] ) {
-    interrupts();
+  if ( !closed[doorN] ) {
     return; // Abort.
   }
-  moving[doorN] = true;
-  interrupts();
 
-  doors[doorN].write( OPEN_ANGLE );
-  delay( MOVE_DELAY );
+  for ( int i = CLOSED_ANGLE + 1; i <= OPEN_ANGLE; i += MOVE_INCREMENT ) {  
 
+    doors[doorN].write( i );
+    delay( MOVE_DELAY );
+    
+  }
+
+  digitalWrite( LED_PINS[doorN], HIGH );
   closed[doorN] = false;
-  moving[doorN] = false;
   
 }
 
+/*
+ * Closes a door.
+ * 
+ * doorN - The number of the door to close.
+ */
 void closeDoor( int doorN ) {
 
-  noInterrupts();
-  if ( moving[doorN] || closed[doorN] ) {
-    interrupts();
+  if ( closed[doorN] ) {
     return; // Abort.
   }
-  moving[doorN] = true;
-  interrupts();
 
-  doors[doorN].write( CLOSED_ANGLE );
-  delay( MOVE_DELAY );
+  for ( int i = OPEN_ANGLE - 1; i >= CLOSED_ANGLE; i -= MOVE_INCREMENT ) {
+    
+    doors[doorN].write( i );
+    delay( MOVE_DELAY );
+    
+  }
 
+  digitalWrite( LED_PINS[doorN], LOW );
   closed[doorN] = true;
-  moving[doorN] = false;
   
 }
 
+/*
+ * Handles a button press, by toggling the state of a door.
+ * 
+ * doorN - The door associated to the pressed button.
+ */
 void buttonPress( int doorN ) {
 
-  if ( closed[doorN] ) {
-    openDoor( doorN );
-  } else {
-    closeDoor( doorN );
-  }
+  shouldToggle[doorN] = true;
   
 }
 
+/*
+ * Sets up the enviroment.
+ */
 void setup() {
 
   for ( int i = 0; i < DOOR_COUNT; i++ ) {
@@ -99,10 +122,12 @@ void setup() {
     doors[i].attach( SERVO_PINS[i] );
     doors[i].write( CLOSED_ANGLE );
 
-    moving[i] = false;
+    shouldToggle[i] = false;
     closed[i] = true;
     
   }
+
+  enableBt(); // Start bluetooth module.
 
   // Register button interrupts.
   attachInterrupt( digitalPinToInterrupt( BUTTON_PINS[0] ), []() { buttonPress( 0 ); }, RISING );
@@ -112,12 +137,24 @@ void setup() {
 
 }
 
+/*
+ * Keeps the LEDs aligned with the state of the associated buttons.
+ */
 void loop() {
 
   for ( int i = 0; i < DOOR_COUNT; i++ ) {
 
-    digitalWrite( LED_PINS[i], digitalRead( BUTTON_PINS[i] ) );
+    if ( shouldToggle[i] ) {
+      if ( closed[i] ) {
+        openDoor( i );
+      } else {
+        closeDoor( i );
+      }
+      shouldToggle[i] = false;
+    }
     
   }
+
+  refresh(); // Update bluetooth state.
 
 }
---
+++ Arduino/Bluetooth.ino
@@ -0,0 +1,81 @@
+#include <cstdio>
+#include <CurieBLE.h>
+
+#define CONNECTION_BUTTON_PIN 6
+#define CONNECTION_LED_PIN 7
+
+#define COUNT_UUID "b4fe2d08-1a07-42bc-bddc-3c0aef4874c6"
+#define CLOSED_UUID_BASE "32191b92-68a4-4c63-a990-5995c5ba2"
+#define TOGGLE_UUID_BASE "ccd1dc5a-c16b-4969-8372-92094b3af"
+
+BLEPeripheral blePeripheral; // This device.
+
+BLEService drawerService( "3205de0b-9f88-4f82-9fa4-590fac4fd4ef" );
+BLEIntCharacteristic countCharacteristic( COUNT_UUID, BLERead );
+BLECharCharacteristic * closedCharacteristic[DOOR_COUNT];
+BLECharCharacteristic * toggleCharacteristic[DOOR_COUNT];
+
+char * getUUID( String base, int n ) {
+
+  char * buf = (char *) malloc( ( base.length() + 3 + 1 ) * sizeof( char ) );
+  sprintf( buf, "%s%03d", base.c_str(), n );
+  return buf;
+  
+}
+
+void enableBt() {
+
+  blePeripheral.setLocalName( "Controllable Drawer" );
+  blePeripheral.setAdvertisedServiceUuid( drawerService.uuid() );
+
+  // Register service and count.
+  blePeripheral.addAttribute( drawerService );
+  blePeripheral.addAttribute( countCharacteristic );
+  countCharacteristic.setValue( DOOR_COUNT );
+
+  for ( int i = 0; i < DOOR_COUNT; i++ ) {
+
+    char * closedUUID = getUUID( CLOSED_UUID_BASE, i );
+    char * toggleUUID = getUUID( TOGGLE_UUID_BASE, i );
+    closedCharacteristic[i] = new BLECharCharacteristic( closedUUID, BLERead | BLENotify );
+    toggleCharacteristic[i] = new BLECharCharacteristic( toggleUUID, BLEWrite );
+    free( closedUUID );
+    free( toggleUUID );
+
+    // Register characteristics.
+    blePeripheral.addAttribute( *( closedCharacteristic[i] ) );
+    blePeripheral.addAttribute( *( toggleCharacteristic[i] ) );
+
+    // Set initial values.
+    closedCharacteristic[i]->setValue( closed[i] ? 1 : 0 );
+    toggleCharacteristic[i]->setValue( 0 );
+    
+  }
+
+  // Begin advertising BLE service.
+  blePeripheral.begin();
+  
+}
+
+void refresh() {
+
+  BLECentral central = blePeripheral.central();
+  if ( central ) {
+
+    for ( int i = 0; i < DOOR_COUNT; i++ ) {
+
+      if ( ( closedCharacteristic[i]->value() != 0 ) != closed[i] ) {
+        closedCharacteristic[i]->setValue( closed[i] ? 1 : 0 );
+      }
+      
+      if ( toggleCharacteristic[i]->value() != 0 ) {
+        shouldToggle[i] = true;
+        toggleCharacteristic[i]->setValue( false );
+      }
+      
+    }
+    
+  }
+  
+}
+
